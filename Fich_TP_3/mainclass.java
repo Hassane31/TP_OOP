d973e0d4a5cd8ec539d4d258700bc6ce5e8674be
Hassan31 <hassanelaabane@gmail.com>
2025-04-09 23:25:16 +0100

    the code is full of errors cuz of my sleepiness

--- Fich_TP_3/mainclass.java
+++ Fich_TP_3/mainclass.java
@@ -2,11 +2,17 @@ package Fich_TP_3;
 import java.util.Scanner;
 class mainclass {
     public static void main(String[] args) {
+
        Scanner input = new Scanner(System.in );
+
        System.out.println("Donner N : ");
+
        int n = input.nextInt();
+
        input.close();
+
        licence somme = new licence();
+
        System.out.println(" S ="+somme.calc(n));
         
     }
---
+++ Fich_TP_4/exo1/main_class.java
@@ -0,0 +1,17 @@
+package Fich_TP_4.exo1;
+
+
+public class main_class {
+    public static void main(String[] args) {
+        class1 car1 =new class1();
+        car1.setCar_modele();
+        System.out.println("--------------------------------");
+       System.out.println("car modele is :"+car1.getCar_modele());
+
+        class1 car2 =new class1();
+        car2.setCar_name;
+        System.out.println("car name is :"+get_name());
+        
+    }
+    
+}
